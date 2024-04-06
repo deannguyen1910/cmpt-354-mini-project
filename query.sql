23daf9057fb0255176c70294d703df392fe614b3
deannguyen1910 <deannguyen1910@gmail.com>
2024-04-05 18:39:41 -0700

    Update final query.py

--- query.sql
+++ query.sql
@@ -1,10 +1,21 @@
 /
-SELECT * FROM Applications
-WHERE ApplicationID == (
-SELECT ApplicationID FROM Review
-WHERE Review.ReviewID == (
-SELECT Researchers.ResearcherID FROM Researchers
-WHERE Researchers.FirstName == 'Emily' AND Researchers.LastName ==  'Young'
+SELECT c.CompetitionID, c.Title
+FROM Competitions c
+INNER JOIN (
+SELECT CompetitionID
+FROM Applications
+WHERE (ApplicationID IN (
+SELECT ApplicationID
+FROM (
+SELECT ApplicationID, COUNT(*) AS participants
+FROM workOn
+GROUP BY ApplicationID
 )
+WHERE participants >= 10
 )
+OR RequestedAmount >= 20000
+)
+) AS a
+ON c.CompetitionID = a.CompetitionID
+WHERE strftime('%m', SubmittedDate) < '12';
 /
